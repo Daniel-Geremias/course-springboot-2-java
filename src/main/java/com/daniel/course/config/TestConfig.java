92e49ad131491920a0794edd110752a1df086306
Daniel-Geremias <daniel.vg19@gmail.com>
2020-03-05 15:56:31 -0300

    Product

--- src/main/java/com/daniel/course/config/TestConfig.java
+++ src/main/java/com/daniel/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.daniel.course.entities.Category;
 import com.daniel.course.entities.Order;
+import com.daniel.course.entities.Product;
 import com.daniel.course.entities.User;
 import com.daniel.course.entities.enums.OrderStatus;
 import com.daniel.course.repositories.CategoryRepository;
 import com.daniel.course.repositories.OrderRepository;
+import com.daniel.course.repositories.ProductRepository;
 import com.daniel.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository; 
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -45,8 +50,15 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 	}
 	
